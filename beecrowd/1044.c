d589dfc8c82069ee22bc2392e4ec18b77a6d24c6
Matheus J <matheus.martins.jeronimo@gmail.com>
2026-02-11 20:05:36 -0300

    Adds 1044

---
+++ beecrowd/1044.c
@@ -0,0 +1,31 @@
+/*
+https://judge.beecrowd.com/pt/problems/view/1044
+
+beecrowd | 1044
+Múltiplos
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+
+Exemplo de Entrada	Exemplo de Saída
+6 24                Sao Multiplos
+
+6 25                Nao sao Multiplos
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a, &b);
+    if( 0 == a%b || 0 == b%a ) printf("Sao Multiplos\n");
+    else printf("Nao sao Multiplos\n");
+    return 0;
+}
