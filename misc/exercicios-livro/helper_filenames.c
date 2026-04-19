a47a2b1ca28fcc5fbc501db77c9a6adac6b6678f
Matheus Martins <matheus.martins.jeronimo@gmail.com>
2026-04-18 22:24:36 -0300

    Add files via upload

--- misc/exercicios-livro/helper_filenames.c
+++ misc/exercicios-livro/helper_filenames.c
@@ -23,9 +23,51 @@ void getDataAtual(char *bufferDataAtual, size_t stringSize)
     strftime(bufferDataAtual, 9, "%Y%m%d", dataDecomposta);
 }
 
+// ....................????????????
+//void getFilenames(char * storeFilenamesAt[],
+//				  size_t 
+//				  const DIR *ponteiroDir,
+//				  const struct dirent *arq) {
+
+
+/*
+ **************************************************************
+ * Fluxo:                                                     *
+ * 1- abrir o diretorio: ponteiro para diretorio atual        *
+ * 2- ler conteudo: loop no diretorio. Cada arquivo eh        *
+ *     representado por um registro/struct                    *
+ * 3- fechar diretorio: fechar/liberar recurso                *
+ *		-> cuidado com memory leak...                         *
+ **************************************************************
+ */
+void getFilenames() {
+
+	DIR *ponteiroParaDiretorio = NULL;
+	ponteiroParaDiretorio = opendir(DIR_TARGET);
+	
+	struct dirent *arquivoInfo = NULL;
+    arquivoInfo = readdir(ponteiroParaDiretorio);
+    
+    do {
+    	arquivoInfo = readdir(ponteiroParaDiretorio);
+    	if( NULL == arquivoInfo ) {
+    		fprintf("Erro reading dir...\n", stderr);
+    		break;
+    	}
+    	// skip . and .. dir
+    	if ( 0 == strcmp(arquivoInfo->d_name, ".")
+    		 || 0 == strcmp(arquivoInfo->d_name, "..") ) {
+    		continue;
+    	}
+    	printf("Nome do arquivo => %s\n", arquivoInfo->d_name);
+    } while ( NULL != arquivoInfo );
+
+	closedir(ponteiroParaDiretorio);
+}
 
 int main()
 {
+	getFilenames();
     DIR *ponteiroDiretorio;
     ponteiroDiretorio = opendir(DIR_TARGET);
 
@@ -44,15 +86,16 @@ int main()
 
     char dataAtual[9];
     char newFilename[100];
+    char filenameWithouExtesion[90];
     *dataAtual = '\0';
     *newFilename = '\0';
+    *filenameWithouExtesion = '\0';
 
     regex_t regex;
     int ret = regcomp(&regex, "^c[0-9][0-9]_e[0-9][0-9].*", REG_EXTENDED);
 
-    char *lista[] = {"abc", "def", "c00_e00.c", "0c00_e00.g"};
+    char *lista[] = {"abc", "c00_00_.hf", "c00_e00.c", "0c00_e00.g"};
 
-    char b[11] = "_";
     for(int i = 0; i < 4; ++i)
     {
         printf("testando: %s\n", lista[i]);
@@ -61,8 +104,39 @@ int main()
         else {
             printf ("validado!\n");
             getDataAtual(dataAtual, 9);
-            snprintf(newFilename, 100, "%s_%s\n", lista[i], dataAtual);
-            printf("novo = %s\n", newFilename);
+            
+            size_t len = strlen(lista[i]);
+            // files .c and .h are valid
+            char extension = lista[i][len - 1];
+            char beforeExtension = lista[i][len - 2];
+            if(beforeExtension == '.'
+               && ( extension == 'c' || extension == 'h' ) )
+            {
+            	printf("eh valido!! 2\n");
+            	
+            	snprintf(newFilename, 100, "%s_%s\n", lista[i], dataAtual);
+                printf("novo = %s\n", newFilename);
+                size_t j;
+                for(j = 0; j < len - 2; ++j) {
+                	*(filenameWithouExtesion + j) = lista[i][j];
+                }
+                *(filenameWithouExtesion + j) = '\0';
+                
+                snprintf(newFilename, 
+                         sizeof(newFilename),
+                         "%s_%s%c%c",
+                         filenameWithouExtesion,
+                         dataAtual,
+                         beforeExtension,
+                         extension);
+                printf("novo = %s\n", newFilename);
+            }
+            
+            
+            
+            
+            
+            
         }
     }
 
